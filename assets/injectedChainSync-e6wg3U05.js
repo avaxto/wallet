31fa33c83e7a781362ee751cad1c976c842bac58
@REKTBuildr <rektbuildr@avax.to>
2026-09-23 14:33:39 -0300

    Update build 0.1.32

--- assets/injectedChainSync-e6wg3U05.js
+++ assets/injectedChainSync-e6wg3U05.js
@@ -1,4 +1,4 @@
-import { g as d, u as f, a as h, b as m, I as u, c as w, __tla as __tla_0 } from "./index-6GsGcl5b.js";
+import { g as d, u as f, a as h, b as m, I as u, c as w, __tla as __tla_0 } from "./index-BmfEsykT.js";
 import "./vendor-SbyuBW_8.js";
 import "./crypto-BGdJyCGl.js";
 let y, k;
